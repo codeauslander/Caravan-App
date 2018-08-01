55b32c7d7287e92c3cd8ab44c3d9ac6851375217
codeauslander <cesar.gusto.catano@gmail.com>
2018-07-31 21:03:24 -0500

    Added unmask and mask issue_type

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,8 +1,35 @@
 class Preference < ApplicationRecord
   belongs_to :user
 
-  def issue_type_pretty
+  TYPES = [
+    'Delay',
+    'Safety',
+    'Cleanliness',
+    'Crowding',
+    'Accessibility',
+    'Staff',
+    'Other',
+  ]
+
+  def type(index)
+    TYPES["#{index}"]
+  end
+
+  def types
+    return TYPES
+  end
+
+  def mask(binary_string)
+    binary_string.to_i(2)
+  end
+
+  def unmask_issue_type
     issue_type.to_s(2).split('')
   end
+
+  
+
+
+
   
 end
